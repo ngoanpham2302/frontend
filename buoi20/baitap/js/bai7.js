45f5cdd84f363cfe8943964e2f04e58dade28137
ngoanpham2302 <ngoanptp2302@gmail.com>
2021-04-19 21:05:35 +0700

    modified buoi20/baitap

--- buoi20/baitap/js/bai7.js
+++ buoi20/baitap/js/bai7.js
@@ -1,10 +1,13 @@
-// Function check chiều rộng màn hình
-$(function () {
-  if (window.matchMedia && window.matchMedia("(max-width: 600px)".matches)) {
+// Function check chiều rộng màn hìnhfunction hideMenu() {
+function hideMenu() {
+  if (window.matchMedia && window.matchMedia("(max-width: 600px)").matches) {
     $(".menu").addClass("hide");
     $(".form").addClass("hide");
   }
-});
+}
+
+window.onload = hideMenu;
+window.onresize = hideMenu;
 
 $(".menu-icon").on("click", function () {
   $(".menu").toggleClass("hide");
